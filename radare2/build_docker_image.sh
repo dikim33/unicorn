f3f5f786b396076f5e01cd633e60ae017f1b27c3
DongInn Kim <dikim@indiana.edu>
2017-06-11 14:56:46 -0400

    Add .gitignore to ignore a temporary dir Minor verbose messages in the script.

--- radare2/build_docker_image.sh
+++ radare2/build_docker_image.sh
@@ -12,9 +12,10 @@ image_name="iuctf/radare2"
 
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
-    echo "$0 requires the your private ssh key as an argument"
+    echo "$0 <your_temporary_private_ssh_key>" 
+    echo "It requires the your private ssh key (RSA) as an argument"
     echo
-    echo "Make sure that your public ssh key is already added to your github profile and"
+    echo "Make sure that your public ssh key (RSA) is already added to your github profile and"
     echo "the paired private ssh key is used here."
     echo "Probably temporary ssh key would be appropriate."
     echo
@@ -39,8 +40,16 @@ if [ ! -d ssh_keys ]; then
     mkdir ssh_keys
 fi
 
-cp $1 ssh_keys/
+cp $1 ssh_keys/id_rsa
 
 docker build -t "$image_name" .
+
+
+if [ $? -ne 0 ]; then
+    echo
+    echo "Oops!!!!"
+    echo "It failed to build your docker image because you may have not provided a correct ssh key"
+    echo
+fi
 
 exit 0;
